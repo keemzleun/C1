fcd16d78503ab97e9b3d9d467fa3050268d4033c
keemzleun <jin9ni0120@naver.com>
2022-11-08 20:56:22 +0900

    hw6

---
+++ hw6/hw6.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h> 
+
+
+int even(int a); 
+int odd(int a);
+
+int even(int a)                
+{
+    if (a % 2 == 0)                 
+        printf("%d ", a);       
+    return 0;
+}
+
+int odd(int a)                 
+{
+    if (a % 2 == 1)                
+        printf("%d ", a);       
+    return 0;
+}
+
+
+int main()                   
+{
+    int i;                    
+    int a[5] = { 0 };            
+
+    printf("Please input five integers: ");      
+
+    for (i = 0; i < 5; i++)                
+    {
+        scanf("%d", &a[i]);             
+    }
+
+    printf("Odd numbers:");                    
+    for (i = 0; i < 5; i++)                
+    {
+        even(a[i]);                  
+    }
+
+    printf("\nEven numbers:");
+    for (i = 0; i < 5; i++)
+    {
+        odd(a[i]);                   
+    }
+
+
+}
